f4dd62aa4aaa9a8ff6b14c39b733786323b2527e
Gwendolyn Zubatch <gyzubatch@gmail.com>
2018-10-30 15:42:15 -0700

    render in work; app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 // build store objects
 
 var stores = [];
+// var dailySalesAllStores=[];
+
 
 function Store(storeName, minHourlyTraffic, maxHourlyTraffic, avgPurchase) {
 
@@ -17,6 +19,7 @@ function Store(storeName, minHourlyTraffic, maxHourlyTraffic, avgPurchase) {
 
   this.populateSalesArray(this.minHourlyTraffic, this.maxHourlyTraffic, this.avgPurchase);
   stores.push(this);
+  dailySalesAllStores.push(this.dailySales);
   this.render();
 }
 
@@ -53,7 +56,7 @@ Store.prototype.populateSalesArray = function(minCust, maxCust, avgCookiesPerCus
     custThisHour = this.getHourlyCustomers(minCust, maxCust);
     salesThisHour = this.getHourlySales(custThisHour, avgCookiesPerCust);
     setClock = this.getHoursOfOp(hoursAfterOpening);
-    this.dailySales[hoursAfterOpening] = {time: setClock, sales: salesThisHour + ' cookies', customers: custThisHour};
+    this.dailySales[hoursAfterOpening] = {time: setClock, sales: salesThisHour, customers: custThisHour};
     console.log('salesThisHour', salesThisHour);
     this.dailyCookieTotal += salesThisHour;
   }
@@ -64,36 +67,79 @@ Store.prototype.render = function () {
   //create elements
   //create content for elements
   //append elements to page at anchor id
-  var mainEl = document.getElementById('main-content');
-  var container = document.createElement('section');
-  var listBoxEl = document.createElement('ul');
-  var listHeadingEl = document.createElement('h3');
-  var totalSalesEl = document.createElement('li');
-  
-  listHeadingEl.textContent = this.storeName;
-  totalSalesEl.textContent = 'Total: ' + this.dailyCookieTotal;
-  
-  listBoxEl.appendChild(listHeadingEl);
 
-  var timeSlot = 0;    // append list items to list box
-  for (var indexDailySales = 0; indexDailySales < this.dailySales.length; indexDailySales++) {
-    timeSlot = document.createElement('li');
-    timeSlot.textContent = this.dailySales[indexDailySales].time + this.dailySales[indexDailySales].sales; // *** CONSIDER ADDING LIST TEXT HERE ***
-    listBoxEl.appendChild(timeSlot);
+  var storeHeaderEl = document.createElement('tr');
+  for (var i=0; i<stores.length; i++) {
+    // create and append store name row header  <th>
+
+    // create and append sales data cells for single store  <td>
+    var thingy = 0;    
+    for (var j=0; j<stores[0].dailySales.length; j++) {    // create and append single hourly sales data element
+      thingy = document.createElement('td');
+      thingy.textContent = stores[i].dailySales[j].sales; // point to sales data for this store at this hour
+      storeHeaderEl.appendChild(document.createElement('td'));
+    }
+
+    // create and append store daily totals row header  <th>
+
   }
-  
-  listBoxEl.appendChild(totalSalesEl);
-  
-  container.appendChild(listBoxEl);
 
-  mainEl.appendChild(container);
+
+
+  // var mainEl = document.getElementById('main-content');
+  // var container = document.createElement('section');
+  // var listBoxEl = document.createElement('ul');
+  // var listHeadingEl = document.createElement('h3');
+  // var totalSalesEl = document.createElement('li');
+  
+  // listHeadingEl.textContent = this.storeName;
+  // totalSalesEl.textContent = 'Total: ' + this.dailyCookieTotal;
+  
+  // listBoxEl.appendChild(listHeadingEl);
+
+  // var timeSlot = 0;    // append list items to list box
+  // for (var indexDailySales = 0; indexDailySales < this.dailySales.length; indexDailySales++) {
+  //   timeSlot = document.createElement('li');
+  //   timeSlot.textContent = this.dailySales[indexDailySales].time + this.dailySales[indexDailySales].sales; // *** CONSIDER ADDING LIST TEXT HERE ***
+  //   listBoxEl.appendChild(timeSlot);
+  // }
+  
+  // listBoxEl.appendChild(totalSalesEl);
+  
+  // container.appendChild(listBoxEl);
+
+  // mainEl.appendChild(container);
 };
 
 // create store objects
-var pikeStore = new Store('1st and Pike', 23, 65, 6.3);
-var seatacStore = new Store('SeaTac Airport', 3, 24, 1.2);
-var seattleCenterStore = new Store('Seattle Center', 11, 38, 3.7);
-var capHillStore = new Store('Capitol Hill', 20, 38, 2.3);
-var alkiStore = new Store('Alki', 2, 16, 4.6);
+new Store('1st and Pike', 23, 65, 6.3);
+new Store('SeaTac Airport', 3, 24, 1.2);
+new Store('Seattle Center', 11, 38, 3.7);
+new Store('Capitol Hill', 20, 38, 2.3);
+new Store('Alki', 2, 16, 4.6);
 
 console.log('stores', stores);
+
+
+// calculate total hourly sales for all stores
+var hourlyTotals = [];
+var thisHourThisStore = 0;
+for (var i=0; i<stores[0].dailySales.length; i++) {
+  thisHourThisStore = 0;
+  for (var j=0; j<stores.length; j++) {
+    thisHourThisStore += parseInt(stores[j].dailySales[i].sales);
+  }
+  hourlyTotals.push(thisHourThisStore);
+}
+console.log('hourlyTotals', hourlyTotals);
+var dailyTotalsAllStores = sumArray(hourlyTotals);
+
+
+// function to sum an array
+function sumArray (someArray) {
+  var arraySum = 0;
+  for (var i=0; i<someArray.length; i++) {
+    arraySum += someArray[i];
+  }
+  return arraySum;
+}
